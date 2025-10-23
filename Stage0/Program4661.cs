4061b04c978e0ccac63efb1a2d3a76c43ef82918
amichai1 <mukades@g.jct.ac.il>
2025-10-23 17:27:05 +0300

    chore(stage0): add Git warm-up console project

---
+++ Stage0/Program4661.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome4661();
+        Welcomeyyyy();
+        Console.ReadKey();
+    }
+
+    static partial void Welcomeyyyy();
+    private static void Welcome4661()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("Enter your name:\n");
+        System.String name = Console.ReadLine() ?? "Guest";
+        Console.WriteLine($"{name}, Welcome to my first console application!");
+    }
+}
